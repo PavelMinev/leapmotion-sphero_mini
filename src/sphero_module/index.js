b8d2da13dcdb394b9c5970841616733892e80fee
PavelMinev <mpl12@rambler.ru>
2021-03-28 10:56:06 +0300

    add calibration with leapmotion

--- src/sphero_module/index.js
+++ src/sphero_module/index.js
@@ -7,6 +7,16 @@ let spheroBall;
 let controller;
 let waitIntervalForNextCommandId;
 let isStopped = true;
+let calibrating = false;
+let headingOffset = 0;
+
+const maxSpeed = 150;
+const minLeft = 0.2;
+const minRight = -0.2;
+const minToward = -0.15;
+const minBackward = 0.2;
+
+const maxLimit = 1;
 
 function getAngle(x, y) {
     return Math.acos(-y / Math.sqrt(Math.pow(x,2)+Math.pow(y, 2))) * 180 / Math.PI * (x <= 0 ? 1 : -1) + (x <= 0 ? 0 : 360);
@@ -21,6 +31,7 @@ const initConnections = async () => {
     spheroBall = await Scanner.findSpheroMini();
     if (!spheroBall) return console.log('sphero mini not available!');
     console.log('Connected to Sphero');
+    spheroBall.setMainLedColor(0xff, 0, 0);
     initLeapMotionConnection();
 }
 
@@ -45,30 +56,43 @@ const initLeapMotionConnection = () => {
     controller.on('frame', frame => {
         const handsKeys = Object.keys(frame.handsMap);
         if (handsKeys[0]) {
-            // console.log(frame.handsMap);
             handleSwipe(frame.handsMap[handsKeys[0]]);
         } else {
             console.log('No hands');
+            stopSphero(spheroBall);
         };
     });
 }
 
 const handleSwipe = hand => {
+    const newCalibrating = hand.fingers[1].extended && !hand.fingers[0].extended && !hand.fingers[2].extended && !hand.fingers[3].extended && !hand.fingers[4].extended;
+    if (calibrating !== newCalibrating) {
+        if (newCalibrating) {
+            spheroBall.setBackLedIntensity(255);
+            spheroBall.setMainLedColor(0, 0, 0);
+        } else {
+            spheroBall.setBackLedIntensity(0);
+            spheroBall.setMainLedColor(0xff, 0, 0);
+        }
+    }
+    calibrating = newCalibrating;
+    if (calibrating) {
+        if (!waitIntervalForNextCommandId) {
+            calibrateSphero(hand.fingers[1].direction[0]);
+            waitIntervalForNextCommandId = setTimeout(() => {
+                clearTimeout(waitIntervalForNextCommandId);
+                waitIntervalForNextCommandId = null;
+            }, WAIT_TIME);
+        }
+        return;
+    }
     let towardsBackwards = hand.pitch();
     let leftRight = hand.roll();
     let previousFrame = controller.frame(1);
-    const towardsBackwardsDeltaColibration = 0;
-    const leftRightDeltaColibration = +0.15;
-    towardsBackwards += towardsBackwardsDeltaColibration;
-    leftRight += leftRightDeltaColibration;
-
-    const maxSpeed = 100;
-    const minLeft = 0.2;
-    const minRight = -0.2;
-    const minToward = -0.15;
-    const minBackward = 0.2;
-
-    const maxLimit = 1;
+    const towardsBackwardsDeltaCalibration = 0;
+    const leftRightDeltaCalibration = +0.15;
+    towardsBackwards += towardsBackwardsDeltaCalibration;
+    leftRight += leftRightDeltaCalibration;
 
     let towardsBackwardsSpeed = 0;
     let leftRightSpeed = 0;
@@ -86,7 +110,6 @@ const handleSwipe = hand => {
     const newSpeed = Math.sqrt(Math.pow(towardsBackwardsSpeed, 2) + Math.pow(leftRightSpeed, 2));
     const newDirection = getAngle(leftRightSpeed, towardsBackwardsSpeed);
     
-    // console.log(newSpeed, newDirection);
     if (!waitIntervalForNextCommandId) {
         moveSphero(newDirection, newSpeed);
         waitIntervalForNextCommandId = setTimeout(() => {
@@ -98,14 +121,26 @@ const handleSwipe = hand => {
 
 const moveSphero = (direction, speed) => {
     console.log(direction, speed);
-    isStopped = true;
+    isStopped = false;
     if (speed !== 0 && !speed) {
       speed = 70;
-    } else if (speed === 0) {
+    } else if (speed === 0 && !calibrating) {
         return stopSphero(spheroBall)
     }
-    spheroBall.heading = direction;
-    spheroBall.roll(speed, direction, []);
+    spheroBall.roll(speed, (direction + headingOffset) % 360, []);
+}
+
+const calibrateSphero = (direction) => {
+    if (Math.abs(direction) > 0.2) {
+        headingOffset += direction * 10;
+        if (headingOffset < 0) {
+            headingOffset += 360;
+        } else if (headingOffset >= 360) {
+            headingOffset -= 360;
+        }
+        console.log(headingOffset);
+        spheroBall.roll(0, headingOffset, []);
+    }
 }
 
 const stopSphero = spheroBall => {
@@ -113,7 +148,8 @@ const stopSphero = spheroBall => {
         return;
     }
     console.log('Stop sphero');
-    isStopped = false;
+    isStopped = true;
+    calibrating = false;
     spheroBall.roll(0, spheroBall.heading || 0, []);
 };
 
